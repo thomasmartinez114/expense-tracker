56cf62538fad7ba7f79d4ce3a5a53eab30fa4054
Martinez <thomas.martinez@igt.com>
2022-09-11 07:21:37 -0400

    TransactionList and useAmount state

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -14,7 +14,7 @@ export const AddTransaction = () => {
             </div>
             <div className="form-control">
                 <label htmlFor="amount">Amount <br />(negative - expense, positive - income)</label>
-                <input type="number" placeholder="Enter amount..." />
+                <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
             </div>
         </form>
   </>;
