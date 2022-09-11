de8f85d427383db205eee861e3a944b0fd20f628
Martinez <thomas.martinez@igt.com>
2022-09-11 07:18:06 -0400

    setText and removed #s

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+
+export const AddTransaction = () => {
+    const [text, setText] = useState('');
+    const [amount, setAmount] = useState(0);
+
+
+    return <>
+        <h3>Add new transaction</h3>
+        <form>
+            <div className="form-control">
+                <label htmlFor="text">Text</label>
+                <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
+            </div>
+            <div className="form-control">
+                <label htmlFor="amount">Amount <br />(negative - expense, positive - income)</label>
+                <input type="number" placeholder="Enter amount..." />
+            </div>
+        </form>
+  </>;
+};
